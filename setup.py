7a11eeefc2bd2abb241b2e957f2349704964c17f
Fred C <github-fred@hidzz.com>
2023-08-22 11:35:55 -0700

    Fix typo

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3.5'
+    'Programming Language :: Python :: 3.5',
     'Topic :: Communications :: Ham Radio',
   ],
 )
